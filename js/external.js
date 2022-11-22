f575d0bf9392d58b434ee12fa4103c0eb90803ea
Matt Guardiola <matt.g.guardiola@gmail.com>
2022-11-22 11:24:43 -0600

    Added 'Javascript with HTML' exersizes

---
+++ js/external.js
@@ -0,0 +1,17 @@
+"use strict"
+
+ console.log(`Hello from external JavaScript`)
+ alert(`Welcome to my Website!`)
+ const color = prompt("What is your favorite color?")
+ alert(`Awesome! ${color} is my favorite color too!`)
+
+
+
+
+ alert(`Lets see how much you have to pay for those movies you rented!`)
+let mermaid = prompt(`How many days did you rent "The Little Mermaid"?`)
+let bear = prompt(`How many days did you rent "Brother Bear?"`)
+let hercules = prompt(`How many days did you rent "Hercules"?`)
+let total = (mermaid * 3) + (bear * 3) + (hercules * 3);
+ alert(`Looks like you owe ${total} Dollars`);
+
